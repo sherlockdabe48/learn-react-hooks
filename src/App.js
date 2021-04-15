c6c74d5144c04b4173d8b4317441ed813a1f9b39
sherlockdabe48 <sherlockdabe48@gmail.com>
2021-04-15 13:31:44 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
+import "./App.css"
+
+const API_URL = "https://jsonplaceholder.typicode.com/"
 
 function App() {
+  const [resourceType, setResourceType] = useState("users")
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch(`${API_URL}${resourceType}`)
+      .then((response) => response.json())
+      .then((json) => setData(json))
+  }, [resourceType])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div>
+        <button onClick={() => setResourceType("users")}>Users</button>
+        <button onClick={() => setResourceType("posts")}>Posts</button>
+        <button onClick={() => setResourceType("comments")}>Comments</button>
+      </div>
+      <h1>{resourceType}</h1>
+      {data.map((d) => {
+        return <pre>{JSON.stringify(d)}</pre>
+      })}
+    </>
+  )
 }
 
-export default App;
+export default App
