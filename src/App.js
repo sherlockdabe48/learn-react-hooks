313abc6cdca96558131cb566fded0c048324ecde
sherlockdabe48 <sherlockdabe48@gmail.com>
2021-04-15 13:45:33 +0700

    finish useEffect

--- src/App.js
+++ src/App.js
@@ -6,14 +6,27 @@ const API_URL = "https://jsonplaceholder.typicode.com/"
 function App() {
   const [resourceType, setResourceType] = useState("users")
   const [data, setData] = useState([])
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
 
   useEffect(() => {
     fetch(`${API_URL}${resourceType}`)
       .then((response) => response.json())
       .then((json) => setData(json))
-  }, [resourceType])
+  }, [])
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setWindowWidth(window.innerWidth)
+    })
+    return () => {
+      window.removeEventListener("resize", () => {
+        setWindowWidth(window.innerWidth)
+      })
+    }
+  }, [windowWidth])
   return (
     <>
+      <h3>{windowWidth}</h3>
       <div>
         <button onClick={() => setResourceType("users")}>Users</button>
         <button onClick={() => setResourceType("posts")}>Posts</button>
