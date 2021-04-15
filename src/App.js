c8dfc67c82732408c476d337055f3f7772428615
sherlockdabe48 <sherlockdabe48@gmail.com>
2021-04-15 14:00:53 +0700

    Change repository

--- src/App.js
+++ src/App.js
@@ -1,41 +1,13 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
 import "./App.css"
-
-const API_URL = "https://jsonplaceholder.typicode.com/"
+import LearnUseEffect from "./components/LearnUseEffect"
+import LearnUseMemo from "./components/LearnUseMemo"
 
 function App() {
-  const [resourceType, setResourceType] = useState("users")
-  const [data, setData] = useState([])
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
-
-  useEffect(() => {
-    fetch(`${API_URL}${resourceType}`)
-      .then((response) => response.json())
-      .then((json) => setData(json))
-  }, [])
-
-  useEffect(() => {
-    window.addEventListener("resize", () => {
-      setWindowWidth(window.innerWidth)
-    })
-    return () => {
-      window.removeEventListener("resize", () => {
-        setWindowWidth(window.innerWidth)
-      })
-    }
-  }, [windowWidth])
   return (
     <>
-      <h3>{windowWidth}</h3>
-      <div>
-        <button onClick={() => setResourceType("users")}>Users</button>
-        <button onClick={() => setResourceType("posts")}>Posts</button>
-        <button onClick={() => setResourceType("comments")}>Comments</button>
-      </div>
-      <h1>{resourceType}</h1>
-      {data.map((d) => {
-        return <pre>{JSON.stringify(d)}</pre>
-      })}
+      <LearnUseEffect />
+      <LearnUseMemo />
     </>
   )
 }
